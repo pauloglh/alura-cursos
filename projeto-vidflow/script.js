213b488b3d0c9abba2310fdb5b9a970e9dba45dc
Paulo Guilherme <130486478+pauloglh@users.noreply.github.com>
2024-04-27 17:23:56 -0300

    Commit de projetos já criados

---
+++ projeto-vidflow/script.js
@@ -0,0 +1,83 @@
+const containerVideos = document.querySelector(".videos__container")
+
+async function buscarEMostrarVideos() {
+    try{//Tratamento de erro
+        const busca = await fetch("http://localhost:3000/videos")
+        const videos = await busca.json();
+            videos.forEach((video) => {
+                if(video.categoria == ""){
+                    throw new Error('O vídeo não possui categoria.')
+                }
+                containerVideos.innerHTML += `
+                    <li class="videos__item">
+                        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                        <div class="descricao-video">
+                            <img class="img-canal" src="${video.imagem} alt="Logo do canal Alura"></img>
+                            <h3 class="titulo-video">${video.titulo}</h3>
+                            <p class="titulo-canal">${video.descricao}</p>
+                        </div>
+                    </li>
+                `;
+            })
+    } catch(error){ //Tratamento de erro
+        containerVideos.innerHTML = `Não foi possível carregar a página: ${error}`;
+    }
+}
+
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+  
+    videos.forEach((video) => {
+      const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+  
+      video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+  }
+  
+  const botaoCategoria = document.querySelectorAll(".superior__item")
+
+  botaoCategoria.forEach((botao) => {
+    let botaoCategoria = botao.getAttribute('name');
+    botao.addEventListener("click",() => filtrarPorCategoria(nomeCategoria));
+  })
+
+function filtrarPorCategoria(filtro){
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos){
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo'){
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
+
+
+  //Outro modelo de lógica para filtrar.
+
+// const videos = document.querySelectorAll(".videos__item")
+    // //Lógica de filtragem abaixo
+    // if (barraDePesquisa.value != "") {
+    //     for(let video of videos){
+    //         let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+    //         let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+    //         if (!titulo.includes(valorFiltro)) {
+    //             video.style.display = "none"
+    //         } else {
+    //             video.style.display = "block"
+    //         }
+    //     }
+    // }else {
+    //     video.style.display = "block"
+    // }
